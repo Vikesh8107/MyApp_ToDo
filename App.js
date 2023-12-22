f5ed2b792e793b8318c3d4a28cfbaf9d6e79048c
Vikesh8107 <anonymsdc123@gmail.com>
2023-12-22 17:00:11 +0530

    Updated completed task page

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import TodoScreen from "./src/screen/TodoScreen";
+import CompletedTasksScreen from "./src/screen/CompletedTasksScreen";
 
-export default function App() {
-	return (
-		<SafeAreaView>
-			<View>
-				<TodoScreen />
-			</View>
-		</SafeAreaView>
-	);
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TodoScreen" component={TodoScreen} />
+        <Stack.Screen
+          name="CompletedTasksScreen"
+          component={CompletedTasksScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
